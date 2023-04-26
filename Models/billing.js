afdfb48a7be2348cf282c43af246f1d47374940e
gregorymark254 <gregorymark254@gmail.com>
2023-04-27 00:57:44 +0300

    controller changes

--- Models/billing.js
+++ Models/billing.js
@@ -29,24 +29,24 @@ module.exports = (sequelize, Sequelize) => {
     }
   });
 
-  // const Payment = sequelize.define("payments", {
-  //   cardName: {
-  //     type: Sequelize.STRING
-  //   },
-  //   cardNumber: {
-  //     type: Sequelize.STRING
-  //   },
-  //   expiryDate: {
-  //     type: Sequelize.STRING
-  //   },
-  //   cvv: {
-  //     type: Sequelize.STRING
-  //   },
-  //   additionalNote: {
-  //     type: Sequelize.STRING
-  //   }
-  // });
+  const Payment = sequelize.define("payments", {
+    cardName: {
+      type: Sequelize.STRING
+    },
+    cardNumber: {
+      type: Sequelize.STRING
+    },
+    expiryDate: {
+      type: Sequelize.STRING
+    },
+    cvv: {
+      type: Sequelize.STRING
+    },
+    additionalNote: {
+      type: Sequelize.STRING
+    }
+  });
 
-  return Billing;
+  return {Billing,payment};
 };
       
